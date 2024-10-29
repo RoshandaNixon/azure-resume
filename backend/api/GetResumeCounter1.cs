590c504973f01ef66261d4c138db728cfb6dd882
Roshanda Nixon <rnixon20@gmail.com>
2024-10-29 12:31:43 -0400

    Attempted to resolve dependencies issues with api

--- backend/api/GetResumeCounter1.cs
+++ backend/api/GetResumeCounter1.cs
@@ -1,41 +1,59 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
 
-namespace Company.Function
+namespace api
 {
-    public static class GetResumeCounter
+    public static class GetResumeCounter1
     {
-        [FunctionName("GetResumeCounter")]
-        public static HttpResponseMessage Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+        [FunctionName("GetResumeCounter1")]
+        public static async Task<HttpResponseMessage> Run(
+            [Microsoft.Azure.Functions.Worker.HttpTrigger(Microsoft.Azure.Functions.Worker.AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "azcloudresume100",
+                containerName: "Counter",
+                Connection = "AzureResumeConnectionString",
+                Id = "1",
+                PartitionKey = "1")] Counter counter,
+            [CosmosDB(
+                databaseName: "azcloudresume100",
+                containerName: "Counter",
+                Connection = "AzureResumeConnectionString")] IAsyncCollector<Counter> counterOut,
             ILogger log)
         {
-            // Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            updatedCounter = counter;
-            updatedCounter.Count += 1;
+            counter.Count += 1;
+            await counterOut.AddAsync(counter);
 
-            var jsonToRetun = JsonConvert.SerializeObject(counter);
+            var jsonToReturn = JsonConvert.SerializeObject(counter);
 
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
             {
-                Content = new StringContent(jsonToRetun, Encoding.UTF8, "application/json")
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
             };
-
-            
         }
+    }
+
+    internal class FunctionNameAttribute : Attribute
+    {
+        private string v;
+
+        public FunctionNameAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+
+    public class Counter
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
     }
 }
 
